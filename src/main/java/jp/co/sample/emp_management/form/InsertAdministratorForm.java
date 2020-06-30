d8667f8e4691545ba98bb562bc4fecd93ada8d9b
yoko-higuchi <yoko.higuchi@rakus.co.jp>
2020-06-30 15:47:22 +0900

    add insert error(管理者登録の入力値が無効の場合にエラー表示させる機能追加)
    add insert error(登録時に無効な入力が確認された場合はエラー表示させる機能追加)
    
    update insert.html(入力エラー時に入力した文字が残るように変更)

--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,5 +1,9 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +12,16 @@ package jp.co.sample.emp_management.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@Size(min = 1, max = 64, message = "１文字以上64文字以下を入力してください")
+	@NotBlank(message = "スペースなどは無効です")
 	private String name;
 	/** メールアドレス */
+	@Email(message = "メールアドレスが不正です")
+	@NotBlank(message = "文字を入力してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message = "スペース等は無効です")
+	@Size(min = 8, max = 24, message = "8文字未満のパスワードは無効です")
 	private String password;
 	public String getName() {
 		return name;
