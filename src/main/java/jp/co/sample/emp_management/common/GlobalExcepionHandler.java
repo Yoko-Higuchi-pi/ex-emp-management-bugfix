5721c9a11a54c6e2ed4085e478840c7f1815d151
yoko-higuchi <yoko.higuchi@rakus.co.jp>
2020-07-02 10:23:49 +0900

    (6-1)実務的エラー処理(404,500)機能追加

---
+++ src/main/java/jp/co/sample/emp_management/common/GlobalExcepionHandler.java
@@ -0,0 +1,21 @@
+package jp.co.sample.emp_management.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class GlobalExcepionHandler implements HandlerExceptionResolver {
+	private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(GlobalExcepionHandler.class);
+	
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
+		LOGGER.error("error!", ex);
+		return null;
+	}
+
+}
